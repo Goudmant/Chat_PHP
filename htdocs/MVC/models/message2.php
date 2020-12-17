48a5c2f56aa6401aaf5a7ecbfa0f29c77f00c539
Goudmant <yvangoudmant@gmail.com>
2020-12-17 17:03:32 +0100

    MVCtestchat2

--- htdocs/MVC/models/message2.php
+++ htdocs/MVC/models/message2.php
@@ -2,6 +2,15 @@
 class message2 extends Model {
 
     var $table = 'message1';
+    
+    function getLast($num=5){
+        
+        return $this->find (array(
+            'limit'=>$num,
+            'order'=>'id DESC'
+
+        ));
+    }
 }
 
 ?>
