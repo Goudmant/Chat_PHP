ceee8dbb713c19bc5e1f49c36038aedcdf57efc1
Goudmant <yvangoudmant@gmail.com>
2020-12-17 17:25:17 +0100

    MVCOK

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,6 +1,19 @@
-<h1>Flo ? <h1>
-<?php echo $content_for_layout; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Chat-PHP</title>
+</head>
+<body>
+    <h1>NavyFlo BlablaChat<h1>
+
+    <?php 
+    echo $content_for_layout; 
+    ?>
 
 
 
-<p> Ici le footer</p>
+    <p>Merci de rester courtois</p>
+</body>
+</html>
